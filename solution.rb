d42ba21b1657a86423b079fa590a7141907e394f
miguelmassv <miguelmassv@hotmail.com>
2018-05-22 20:01:55 -0500

    reto 2 listo

--- solution.rb
+++ solution.rb
@@ -1,7 +1,17 @@
 require 'sinatra'
-
-get '/:nombre' do
-    "hola #{params[:nombre].capitalize}!"
-end
-
-
+get '/' do
+    "Hola desconocido!"
+  end
+  
+  
+  get '/makers' do
+    "Hola desconocido!"
+  end
+  
+  get '/makers/:nombre' do
+    if (params[:nombre]==nil || params[:nombre]=={} || params[:nombre]=="")
+      "Hola desconocido!"
+    else
+      "<h1>Hola "+"#{params[:nombre]}!</h1>".capitalize
+    end
+  end
