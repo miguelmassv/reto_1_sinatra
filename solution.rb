5ca93efef0c846049a71363c5f652227b2fcc9ff
miguelmassv <miguelmassv@hotmail.com>
2018-05-22 19:55:42 -0500

    reto 2 listo

--- solution.rb
+++ solution.rb
@@ -1,11 +1,7 @@
 require 'sinatra'
 
-get '/' do
-    if (params[:nombre]==nil || params[:nombre]=={} || params[:nombre]=="")
-      "Hola desconocido!"
-    else
-      "<h1>Hola #{params[:nombre]}!</h1>"
-    end
-  end
+get '/:nombre' do
+    "hola #{params[:nombre].capitalize}!"
+end
 
-# http://localhost:4567/?nombre=miguel
+
