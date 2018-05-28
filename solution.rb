ae8441115bc503e7122f5045249f7d3257f86b37
miguelmassv <miguelmassv@hotmail.com>
2018-05-28 15:32:21 -0500

    listo

--- solution.rb
+++ solution.rb
@@ -1,14 +1,7 @@
 require 'sinatra'
 
-get '/' do
-  erb :index
-end
 
-post '/abuelita' do
-  if params[:cantaleta] == params[:cantaleta].upcase
-    @cantaleta = "Ahhh si, manzanas!"
-  else
-    @cantaleta = "Habla más duro mijito"
-  end
-  erb :abuelita
+get '/' do
+    @navegador= request.user_agent
+    "#{@navegador}"
 end
